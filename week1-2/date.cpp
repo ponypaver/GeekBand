246108cb9e797a4eb4336e97525c3141bfc84595
test <test@git.com>
2015-09-14 03:14:47 -0400

    romve debug from date

--- week1-2/date.cpp
+++ week1-2/date.cpp
@@ -3,15 +3,8 @@
 #include <ctime>
 #include "date.h"
 
-#define debug 0
-
 int main(void)
 {
-#if debug
-    date ad[5] = {date(1,0,6), date(1,0,1), date(1,1,2), date(1,0,3), date(1,0,4)};
-    Sort(ad, 5);
-    print(ad, 5);
-#endif
     const int n = 10;
     date d1, *pd = NULL;
     std::cout<< "Create " << n << " random dates: " << std::endl;
