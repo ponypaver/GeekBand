b1bb4e1b55f847b52fa044bd6551b1d0d64e7e23
test <test@git.com>
2015-09-24 06:05:02 -0400

    try another method

--- week3/shape.cpp
+++ week3/shape.cpp
@@ -5,21 +5,6 @@
 #include "circle.h"
 #include "rectangle.h"
 
-void Sort_area(Shape *p[])
-{
-    Shape *tmp = NULL;
-
-    for (int i = 0; i < 20; ++i)
-        for (int j = i+1; j < 20; ++j)
-        {
-            if (p[i]->getArea() < p[j]->getArea()) {
-            tmp = p[i];
-            p[i] = p[j];
-            p[j] = tmp;
-            }
-        }
-}
-
 void print(Shape *s[], int n)
 {
     for (int i = 0; i < n; ++i)
@@ -27,14 +12,16 @@ void print(Shape *s[], int n)
 
 }
 
-int removeshape(Shape *s[])
+int removeshape(Shape *s[], Shape* d[])
 {
-    int remain;
+    int remain = 0, j = 0;
     for (int i = 0; i < 20; ++i)
     {
         if (s[i]->getArea() >= 50)
         {
-            remain = i + 1;
+            remain++ ;
+            d[j++] = s[i];
+            s[i] = NULL;
             continue;
         }
         else
@@ -50,6 +37,8 @@ int main(void)
 {
     int remain;
     Shape* s[20];
+    Shape* d[20];
+
     srand(time(NULL));
 
     for (int n = 0, i = 0; i < 20; i+=2)
@@ -58,11 +47,10 @@ int main(void)
         s[i+1] = new Circle(++n, rand()%10+1, rand()%10+1, rand()%10+1);
     }
     std::cout << "Oringal 20 shapes:" << std::endl;
-    Sort_area(s);
     print(s, 20);
-    remain = removeshape(s);
+    remain = removeshape(s, d);
     std::cout << std::endl << "After removed the Shapes whose area less than 50" << std::endl;
-    print(s, remain);
+    print(d, remain);
 
     return 0;
 }
